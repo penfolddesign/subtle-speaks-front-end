d204d5ee8e87e667cf4a2c7bbc55a93f24074ea7
Aaron <aaron@penfolddesign.co.uk>
2021-02-19 20:09:52 +0000

    added undefined window

--- src/pages/project-aces.js
+++ src/pages/project-aces.js
@@ -14,43 +14,15 @@ import "react-lightbox-component/build/css/index.css";
 
 
 
-var images = [
-
-    {
-        src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404437/didyouknow_4c2a736b76.jpg',
-        title: 'image'
-    },
-    {
-        src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/notapartofourculture_c996b93d47.jpg'
-    },
-    {
-        src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/APRIL_MONTH_gram_b798ac8d7d.jpg'
-    },
-    {
-        src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/6_02f6310bab.jpg'
-    },
-    {
-        src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/what_we_know_fb560529c4.png'
-    },
-    {
-        src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/4_CLUBS_5edf6b6751.jpg'
-    },
-
-
-
-
-
-]
-
-
-
-const ProjectACES= ({data}) => {
+export default function ProjectACES({data}){
 
 
 
     const result = data.epc
 
-
+    if (typeof window === "undefined") {
+        return <p>Server Render</p>
+      }
     return (
         <Layout>
                <ProjectBanner
@@ -73,14 +45,48 @@ const ProjectACES= ({data}) => {
                       <div className="aces-blurb">
                         <video src="https://res.cloudinary.com/dhh2ejp7p/video/upload/v1612018734/FINAL_0dce5e5291.mp4" controls></video>
                         <div className="deck-of-cards-lightbox">
-                                {/* <Lightbox images={images} showImageModifiers={false}/> */}
+                                <Lightbox images={[
+
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613478033/Dance_Is_Freedom_14c0519531.jpg',
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613478033/Did_You_Know_59257d686a.jpg'
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613478033/Consent_Is_Everything_496b26129b.jpg'
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/4_CLUBS_5edf6b6751.jpg',
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/notapartofourculture_c996b93d47.jpg'
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/what_we_know_fb560529c4.png'
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/6_02f6310bab.jpg'
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613478253/didyouknow_4c2a736b76.jpg'
+},
+{
+    src: 'https://res.cloudinary.com/dhh2ejp7p/image/upload/v1613404436/APRIL_MONTH_gram_b798ac8d7d.jpg'
+},
+
+
+
+
+
+]} showImageModifiers={false}/>
                         </div>
                         
                         </div>
                       </div>
                    </div>
-                <div className="container">
-                    
+                <div className="container aces-box-container">
+               <p>By acknowledging the influence of inappropriate behavior that is embedded within our culture we are working towards a technological solution that provides operators with the opportunity to improve customer safety through:</p>
                 <ProjectBoxContainer project="#AFE1FC">
 
 <ProjectBox
@@ -107,7 +113,6 @@ color="#5388B8"
 
 </ProjectBoxContainer>
                 </div>
-                 
                    <div className="project-about aces-about">
                   <div className="container">
                   <Markdown source={result.Description} escapeHtml={true}/>
@@ -136,7 +141,6 @@ color="#5388B8"
 }
 
 
-export default ProjectACES
 
 export const query = graphql`
 
